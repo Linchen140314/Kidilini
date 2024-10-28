28e6a74f0e20d62501680168aff03992a46c66a2
JacquelineSynax <148044421+Linchen140314@users.noreply.github.com>
2024-10-28 12:57:22 +0100

    push 2

--- Color.swift
+++ Color.swift
@@ -1,17 +1,8 @@
 //
 //  Color.swift
-//  Kidsapp
+//  KidsApp
 //
-//  Created by Jacqueline Kolbe on 28.08.24.
+//  Created by Jacqueline Kolbe on 28.10.24.
 //
 
 import Foundation
-import SwiftUI
-
-
-extension Color {
-    static let turquoise = Color(red: 9 / 255, green: 204 / 255, blue: 191 / 255)
-    static let gelb = Color(red: 255 / 255, green: 203 / 255, blue: 123 / 255)
-    static let lila = Color(red: 120 / 255, green: 121 / 255, blue: 235 / 255)
-    static let grau = Color(red: 55 / 255, green: 55 / 255, blue: 55 / 255)
-}
